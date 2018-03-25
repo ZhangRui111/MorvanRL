51df42d36731b259dc87cf20178066d359a42cfa
ZhangRui111 <zhangruisg111@163.com>
2018-03-25 20:18:45 +0800

    Sarsa

--- 02_Q_learning_maze/RL_brain.py
+++ 02_Q_learning_maze/RL_brain.py
@@ -1,6 +1,7 @@
 """
 This part of code is the Q learning brain, which is a brain of the agent.
 All decisions are made in here.
+
 View more on my tutorial page: https://morvanzhou.github.io/tutorials/
 """
 
@@ -10,7 +11,7 @@ import pandas as pd
 
 class QLearningTable:
     def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
-        self.actions = actions  # a list
+        self.actions = actions  # a list ['u', 'd', 'l', 'r']
         self.lr = learning_rate
         self.gamma = reward_decay
         self.epsilon = e_greedy
@@ -22,7 +23,16 @@ class QLearningTable:
         if np.random.uniform() < self.epsilon:
             # choose best action
             state_action = self.q_table.loc[observation, :]
-            state_action = state_action.reindex(np.random.permutation(state_action.index))     # some actions have same value
+            # some actions may have same value.
+            # # DataFrame.idxmax(axis=0, skipna=True)
+            # Return index of first occurrence of maximum over requested axis. So if there are some
+            # actions with same value, we need to shuffle all actions before we choose the idxmax action.
+            state_action = state_action.reindex(np.random.permutation(state_action.index))
+            # # numpy.random.permutation(x)
+            # # DataFrame.reindex(......)
+            # Conform DataFrame to new index with optional filling logic, placing NA/NaN in locations having
+            # no value in the previous index. A new object is produced unless the new index is equivalent to
+            # the current one and copy=False
             action = state_action.idxmax()
         else:
             # choose random action
@@ -48,3 +58,22 @@ class QLearningTable:
                     name=state,
                 )
             )
+
+# # numpy.random.permutation(x)
+# Randomly permute a sequence, or return a permuted range.
+# If x is a multi-dimensional array, it is only shuffled along its first index.
+#
+# Parameters:
+# x : int or array_like
+# If x is an integer, randomly permute np.arange(x). If x is an array, make a copy and shuffle
+# the elements randomly.
+
+# >>> np.random.permutation(10)
+# array([1, 7, 4, 3, 0, 9, 2, 5, 8, 6])
+# >>> np.random.permutation([1, 4, 9, 12, 15])
+# array([15,  1,  9,  4, 12])
+# >>> arr = np.arange(9).reshape((3, 3))
+# >>> np.random.permutation(arr)
+# array([[6, 7, 8],
+#        [0, 1, 2],
+#        [3, 4, 5]])
